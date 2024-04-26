46880e12ed26be7b3803e3adedb1d4cc17ed925b
yotamlevit <yotam.levit@gmail.com>
2024-04-26 12:00:30 +0300

    Added print function

--- set.c
+++ set.c
@@ -2,11 +2,13 @@
   Created by Yotam Levit on 25/04/2024.
 */
 
+#include "stdio.h"
+
 #include "set.h"
 
 
+
 void printSet(Set* s) {
-    printf("Data: ");
     for (int i = 0; i < 16; i++) {
         printf("%02x ", (unsigned char)s->setData[i]);
     }
