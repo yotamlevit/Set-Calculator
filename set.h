a0469807babfd49afec0639d27b4430e2b7da65c
yotamlevit <yotam.levit@gmail.com>
2024-05-14 17:01:19 +0300

    Change functions headers and add constants

--- set.h
+++ set.h
@@ -5,21 +5,29 @@
 #ifndef C_ALGORITHEMS_SET_H
 #define C_ALGORITHEMS_SET_H
 
+#define BYTE_SIZE 8
+#define END_OF_LIST (-1)
+#define MAX_NUMBER 128
+#define ARRAY_DATA_SIZE 16
+
+#define SINGLE_DIGIT_NUMBERS 10
+#define DOUBLE_DIGIT_NUMBERS MAX_NUMBER - SINGLE_DIGIT_NUMBERS - (MAX_NUMBER - 100) //90
+#define TRIPLE_DIGIT_NUMBER MAX_NUMBER - 99
 
 typedef struct set
 {
-    char setData[16];
-} Set;
+    unsigned char setData[ARRAY_DATA_SIZE];
+} Set, * SetPtr;
 
 
 /* Function Headers */
-void initSet(Set* set);
-void printSet(Set* set);
-void readSet(Set* set, int* numbers);
-Set* unionSet(Set* originalSet, Set* mergeSet);
-Set* intersectSet(Set* originalSet, Set* intersectSet);
-Set* subSet(Set* originalSet, Set* subSet);
-Set* symDiffSet(Set* originalSet, Set* subSet);
+void initSet(SetPtr set);
+char* toString(SetPtr set);
+void readSet(SetPtr set, int* numbers);
+void unionSet(SetPtr setA, SetPtr setB, SetPtr outputSet);
+void intersectSet(SetPtr setA, SetPtr setB, SetPtr outputSet);
+void subSet(SetPtr originalSet, SetPtr subSet, SetPtr outputSet);
+void symDiffSet(SetPtr setA, SetPtr setB, SetPtr outputSet);
 
 
 
