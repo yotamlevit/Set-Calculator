5ca976953a035825176f2c70d4b7d38c1a076532
yotamlevit <yotam.levit@gmail.com>
2024-05-16 22:07:18 +0300

    Added command favtory to main loop

--- myset.c
+++ myset.c
@@ -2,12 +2,19 @@
   Created by Yotam Levit on 25/04/2024.
 */
 
-#include "stdio.h"
+#include <stdio.h>
+#include <strings.h>
 
 #include "set.h"
 #include "Errors.h"
+#include "CommandFactory.h"
 
+#define WELCOME_MSG "Hi, Welcome to MMN22 by Yotam Levit\n---------------------------------\n\n"
+#define EXIT_MSG "\nThank you for using MMN22\nBye...\n"
 #define MAXLEN 1200 /* Define maximum length for commands and inputs */
+#define EXIT_COMMAND "stop"
+
+#define REMOVE_NEW_LINE(str) *strchr(str, '\n') = '\0'
 
 typedef enum {
     SETA,
@@ -29,6 +36,11 @@ void initAllSets(SetPtr sets)
 }
 
 
+void func(){
+    printf("asdasd");
+}
+
+
 
 int main() {
     //Set SETA, SETB, SETC, SETD, SETE, SETF;
@@ -38,6 +50,9 @@ int main() {
     initAllSets(allSets);
 
     char command[MAXLEN];
+    FunctionPointer commandFunction;
+    int r;
+
 
     /* Define a 2Dim array of commands name */
     char commands[][MAXLEN] = {
@@ -51,18 +66,38 @@ int main() {
     };
 
     /* Print a welcome message */
-    printf("Hi, Welcome to MMN22 by Yotam Levit\n---------------------------------\n\nEnter Command:\n");
-/*
-    while (fgets(command, MAXLEN, stdin))
-    {
+    printf(WELCOME_MSG);
 
-    }*/
-
-    printf("%s\n\n", getError(undefinedCommandNameError)->error_msg);
     int arr[128] = {1,2, 3, 4,5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 126, -1}; //{1,2,3,4,5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 126, -1};
     int arr2[128] = {1,2,3, 110,125, -1};
     readSet(&allSets[SETA], arr);
     readSet(&allSets[SETB], arr2);
+
+
+    do {
+        printf("Enter Command: ");
+        fgets(command, MAXLEN, stdin);
+        REMOVE_NEW_LINE(command);
+
+        commandFunction = getCommand(command);
+
+        printf("\n%s\n", (char * )commandFunction(&allSets[SETA]));
+
+        // Parse Command
+    }while(strcmp(command, EXIT_COMMAND));
+
+
+    printf(EXIT_MSG);
+
+
+
+
+
+
+    printf("%s\n\n", getError(undefinedCommandNameError)->error_msg);
+
+
+    /*
     printf("\n%s\n", printSet(&allSets[SETA]));
     printf("\n%s\n", printSet(&allSets[SETB]));
 
@@ -76,7 +111,7 @@ int main() {
     printf("\n%s\n", printSet(&allSets[SETC]));
 
     symDiffSet(&allSets[SETA], &allSets[SETB], &allSets[SETC]);
-    printf("\n%s\n", printSet(&allSets[SETC]));
+    printf("\n%s\n", printSet(&allSets[SETC]));*/
 
     return 0;
 }
